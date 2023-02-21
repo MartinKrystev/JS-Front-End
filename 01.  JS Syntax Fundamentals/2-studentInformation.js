5efd82561ef824492f4b82023992838bdbc2db87
MartinKrystev <martin.krystev@gmail.com>
2023-02-21 20:58:50 +0200

    Create 2-studentInformation.js

---
+++ 01.  JS Syntax Fundamentals/2-studentInformation.js
@@ -0,0 +1,10 @@
+// let name = 'John';
+// let age = 15;
+// let grade = 5.54678;
+
+function studentInfo(name, age, grade){
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`);
+}
+
+// studentInfo(name, age, grade);
+
