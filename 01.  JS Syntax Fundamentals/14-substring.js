cd178af737a0363ec81c27ceab1123fabdc9f222
MartinKrystev <martin.krystev@gmail.com>
2023-02-23 18:48:53 +0200

    Create 14-substring.js

---
+++ 01.  JS Syntax Fundamentals/14-substring.js
@@ -0,0 +1,11 @@
+// let string = 'SkipWord';
+// let startIndex = 4;
+// let count = 2;
+
+function func(string, startIndex, count) {
+
+    let result = string.substring(startIndex, startIndex + count);
+    console.log(result);
+}
+
+// func(string, startIndex, count);
