9f2b109789c4e438a7a3146633dbfc8db5f5518c
MartinKrystev <martin.krystev@gmail.com>
2023-02-24 17:39:43 +0200

    Create 5-mathOperations.js

---
+++ 02. JS Syntax Fundamentals - Exercise/5-mathOperations.js
@@ -0,0 +1,20 @@
+function calculation(a, b, operator) {
+
+    let result = 0;
+    switch(operator) {
+        case '+': result = a + b;
+            break;
+        case '-': result = a - b;
+            break;
+        case '*': result = a * b;
+            break;
+        case '/': result = a / b;
+            break;
+        case '%': result = a % b;
+            break;
+        case '**': result = a ** b;
+            break;
+    }
+
+    console.log(result);
+}
