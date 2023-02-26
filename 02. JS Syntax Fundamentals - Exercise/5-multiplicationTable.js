be8fdbc298d5ae4c9d29ff1bdaf7ebff660c4df1
MartinKrystev <martin.krystev@gmail.com>
2023-02-26 21:38:59 +0200

    Create 5-multiplicationTable.js

---
+++ 02. JS Syntax Fundamentals - Exercise/5-multiplicationTable.js
@@ -0,0 +1,10 @@
+function multiply(number){
+    
+    for(let i = 1; i <= 10; i++){
+        let result = number * i;
+
+        console.log(number.toString() + ' X ' + i + ' = ' + result);
+    }
+}
+
+// multiply(5)
