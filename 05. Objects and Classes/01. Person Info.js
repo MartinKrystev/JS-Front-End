e693a6e003d6d0e7331ec4eab29356ae1147d666
MartinKrystev <martin.krystev@gmail.com>
2023-03-09 11:56:10 +0200

    Create 01. Person Info.js

---
+++ 05. Objects and Classes/01. Person Info.js
@@ -0,0 +1,11 @@
+function personInfo(first, second, third) {
+    let person = {};
+
+    person.firstName = first;
+    person.lastName = second;
+    person.age = Number(third);
+
+    return person;
+}
+
+// personInfo('Dark', 'Templar', 1732);
