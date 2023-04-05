09e5e806ae947cab96661e43ae2b68df60a172d4
MartinKrystev <martin.krystev@gmail.com>
2023-04-05 19:33:32 +0300

    Subtraction

---
+++ 08. DOM and Events - Exercise/01. Subtraction/subtract.js
@@ -0,0 +1,7 @@
+function subtract() {
+    let numOne = document.getElementById('firstNumber').value;
+    let numTwo = document.getElementById('secondNumber').value;
+
+    let result = document.getElementById('result');
+    result.textContent = Number(numOne) - Number(numTwo);
+}
